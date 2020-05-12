cb2fab7c3f121d98938e485c851c2eb36314fc33
Benjamin Bowo <ben.bowo@gmail.com>
2020-05-12 10:44:01 +0300

    Updates

---
+++ precache-manifest.62d5ea984f82ddeb2dbe5d624904bf4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89fb12cc69cad8ea7b096958cb07d006",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "332f0dc7d38072d2688a",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/portfolio/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "332f0dc7d38072d2688a",
+    "url": "/portfolio/static/js/main.02d1b9b5.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
