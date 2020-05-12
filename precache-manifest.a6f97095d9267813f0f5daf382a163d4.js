738e10a18247b8797344188c0eedc8be8d7e9267
Benjamin Bowo <ben.bowo@gmail.com>
2020-05-12 14:49:54 +0300

    Updates

---
+++ precache-manifest.a6f97095d9267813f0f5daf382a163d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927ac36cea90b4121ab4d2fcd14f3204",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "375e58a0b3713ad1568f",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "b2fc13bbcaf6e1c4a972",
+    "url": "/portfolio/static/css/main.81d600dc.chunk.css"
+  },
+  {
+    "revision": "375e58a0b3713ad1568f",
+    "url": "/portfolio/static/js/2.03e251c0.chunk.js"
+  },
+  {
+    "revision": "cf7302ab57415842ba2bf20f719dee93",
+    "url": "/portfolio/static/js/2.03e251c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2fc13bbcaf6e1c4a972",
+    "url": "/portfolio/static/js/main.0aa2de48.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "c473d2629c4f77a7cd9728dd5fa8c5d7",
+    "url": "/portfolio/static/media/profilepic.c473d262.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.25beb4db6e09f5726f176c5318abbf96.js"
+  "/portfolio/precache-manifest.a6f97095d9267813f0f5daf382a163d4.js"
 );
 
 self.addEventListener('message', (event) => {
